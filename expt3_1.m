ce8c80482fba9b913d134d0ce5c30036cc875a7c
Dark-Supreme <35609779+Dark-Supreme@users.noreply.github.com>
2020-06-11 12:18:09 +0530

    Delete expt3_1.m

--- expt3_1.m
+++
@@ -1,7 +0,0 @@
-%Time response of 1st and 2nd order system %
-% Find the time response of the given system subjected to unit step input %
-% tf= 1/(7.26s+1)%
-
-num=[1];
-den=[7.26 1];
-step(num,den);
