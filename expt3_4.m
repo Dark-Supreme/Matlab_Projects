58a26e5cf7ccc0d838c9e7802471b5800266dd23
Dark-Supreme <35609779+Dark-Supreme@users.noreply.github.com>
2020-06-11 12:18:25 +0530

    Delete expt3_4.m

--- expt3_4.m
+++
@@ -1,9 +0,0 @@
-% Response of a 2nd order closed loop system %
-
-% wn=5 rad/sec  damping ratio=0.4  %
-
-wn=5;
-damping_ratio=0.4;
-[num0,den]=ord2(wn,damping_ratio);
-num=5^2*num0;
-printsys(num,den,'w');  % 'w' takes the place of 's' in the transfer function %
